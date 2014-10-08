6c940502f181bef73cbf42183eefb9bbcda1c85c
fbm-static <fbm-static@users.noreply.github.com>
2014-10-08 08:58:44 +0800

    feature add: context.menu

--- main.js
+++ main.js
@@ -1,50 +1,63 @@
-(function() {
-  'use strict';
+var currentWindow;
 
-  var currentWindow = null;
+chrome.commands.onCommand.addListener(function(command) {
+  if (command !== 'generate_qr_code') return;
 
-  chrome.commands.onCommand.addListener(function(command) {
-    if (command !== 'generate_qr_code') return;
+  console.log('shit');
 
-    chrome.tabs.executeScript({
-      code: 'window.getSelection().toString();'
-    }, function(selection) {
-      if (selection[0]) return renderCode(selection[0]);
+  chrome.tabs.executeScript({
+    code: 'window.getSelection().toString();'
+  }, function(result) {
+    if (result && result[0]) {
+      renderCode(result[0]);
+      return;
+    }
 
-      chrome.tabs.query({ active: true }, function(tabs) {
-        if (tabs.length) renderCode(tabs[0].url);
-      });
+    chrome.tabs.query({ active: true }, function(tabs) {
+      if (tabs.length) renderCode(tabs[0].url);
     });
   });
+});
 
-  chrome.windows.onRemoved.addListener(function (windowId) {
-    if (currentWindow && windowId === currentWindow.id) {
-      currentWindow = null;
-    }
-  });
+chrome.contextMenus.create({
+  title: 'Generate QR Code',
+  id: 'simpleQrCodeG',
+  contexts: ['all']
+});
 
-  function renderCode(text) {
-    var options = {
-      type: 'popup',
-      width: 300,
-      height: 380,
-      focused: true,
-      url: 'render.html'
-    };
+chrome.contextMenus.onClicked.addListener(function (req) {
+  if (req.menuItemId !== 'simpleQrCodeG') return;
+  renderCode(req.selectionText || req.linkUrl || req.pageUrl);
+});
 
-    console.log(text);
-
-    if (currentWindow) {
-      chrome.windows.update(currentWindow.id, { focused: true }, sendMessage);
-    } else {
-      chrome.windows.create(options, sendMessage);
-    }
-
-    function sendMessage(newWindow) {
-      currentWindow = newWindow;
-      setTimeout(function() {
-        chrome.runtime.sendMessage(text);
-      }, 100);
-    }
+chrome.windows.onRemoved.addListener(function (windowId) {
+  if (currentWindow && windowId === currentWindow.id) {
+    currentWindow = null;
   }
-})();
+});
+
+function renderCode(text) {
+  var options = {
+    type: 'popup',
+    width: 300,
+    height: 380,
+    focused: true,
+    url: 'render.html'
+  };
+
+  console.log('text is %j', text);
+
+  if (currentWindow) {
+    chrome.windows.update(currentWindow.id, { focused: true }, sendMessage);
+  } else {
+    chrome.windows.create(options, sendMessage);
+  }
+
+  function sendMessage(newWindow) {
+    currentWindow = newWindow;
+    chrome.runtime.sendMessage({
+      method: 'renderText',
+      text: text
+    });
+  }
+}
--- render.js
+++ render.js
@@ -1,13 +1,14 @@
 var render = document.getElementById('render');
 var text = document.getElementById('text');
 
-chrome.runtime.onMessage.addListener(function(message) {
-  console.log(message);
-  text.innerHTML = message;
+chrome.runtime.onMessage.addListener(function(req) {
+  if (req.method !== 'renderText') return;
+  console.log('the message is', req.text);
+  text.innerHTML = req.text;
   text.style.border = '1px solid lightblue';
   render.innerHTML = '';
   new QRCode(render, {
-    text: message,
+    text: req.text,
     width: 256,
     height: 256
   });
